8ba581c50ec58eb358ff049a72a296f4c7f4d85b
jahansha-ahammed <jahanshaahammed5@gmail.com>
2026-09-13 22:35:33 +0530

    Update 01_beginner_questions.sql

--- queries/01_beginner_questions.sql
+++ queries/01_beginner_questions.sql
@@ -15,3 +15,18 @@ select * from orders where status='delivered';
 
 #Find customers who signed up in 2025.
 select * from customers where signup_date >= '2025-01-01' and signup_date < '2026-01-01';
+
+#Find the 10 cheapest products.
+select * from products order by price asc limit 10;
+
+#Show the 10 most recent orders.
+select * from orders order by order_date desc limit 10;
+
+#Show the 10 most recent orders.
+select * from orders order by order_date desc limit 10;
+
+#List all products priced above ₹1,000.
+select * from products where price > 1000;
+
+#List all orders with status = 'shipped'.
+select * from orders where status='shipped';
